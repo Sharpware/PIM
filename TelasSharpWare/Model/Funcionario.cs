8f99728716076304a249cd3e1f11eda9ea9fac3e
George Alves da Silveira <georgealvessilveira@outlook.com>
2015-09-24 18:35:00 -0300

    criação das classes Pessoa, Cliente, Funcionario e enum TipoFuncionario

---
+++ TelasSharpWare/Model/Funcionario.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelasSharpWare.Model
+{
+    public class Funcionario : Pessoa
+    {
+        private String login;
+        private String senha;
+
+        public string Login
+        {
+            get
+            {
+                return login;
+            }
+
+            set
+            {
+                login = value;
+            }
+        }
+
+        public string Senha
+        {
+            get
+            {
+                return senha;
+            }
+
+            set
+            {
+                senha = value;
+            }
+        }
+    }
+}
---
+++ TelasSharpWare/Model/Pessoa.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelasSharpWare.Model
+{
+    public abstract class Pessoa
+    {
+        private long id;
+        private String nome;
+        private long cpf;
+        private DateTime dataNascimento;
+        private String telefonePrimario;
+        private String telefoneSecundario;
+        private String celular;
+        private String observacao;
+
+        public long Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+
+            set
+            {
+                nome = value;
+            }
+        }
+
+        public long Cpf
+        {
+            get
+            {
+                return cpf;
+            }
+
+            set
+            {
+                cpf = value;
+            }
+        }
+
+        public DateTime DataNascimento
+        {
+            get
+            {
+                return dataNascimento;
+            }
+
+            set
+            {
+                dataNascimento = value;
+            }
+        }
+
+        public string TelefonePrimario
+        {
+            get
+            {
+                return telefonePrimario;
+            }
+
+            set
+            {
+                telefonePrimario = value;
+            }
+        }
+
+        public string TelefoneSecundario
+        {
+            get
+            {
+                return telefoneSecundario;
+            }
+
+            set
+            {
+                telefoneSecundario = value;
+            }
+        }
+
+        public string Celular
+        {
+            get
+            {
+                return celular;
+            }
+
+            set
+            {
+                celular = value;
+            }
+        }
+
+        public string Observacao
+        {
+            get
+            {
+                return observacao;
+            }
+
+            set
+            {
+                observacao = value;
+            }
+        }
+    }
+}
