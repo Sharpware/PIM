360d541144a2502948895472bea1f3a88a23a57d
Rodrigo Cardoso <rodrigo.fcardoso@hotmail.com>
2015-10-23 00:57:39 -0200

    att venda

--- TelasSharpWare/DAO/VendaDao.cs
+++ TelasSharpWare/DAO/VendaDao.cs
@@ -4,13 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TelasSharpWare.Model;
+using MySql.Data.MySqlClient;
 
 namespace TelasSharpWare.DAO
 {
     public class VendaDao
     {
-        public VendaDao()
+        MySqlConnection _con;
+        public VendaDao(MySqlConnection con)
         {
+            _con = con;
+        }
+
+        public void SalvarVenda(Venda venda)
+        {
+            try
+            {
+                string cmdSalvarVenda = @"insert into venda (valor_total, tipo_venda, data_venda)
+                                        values (@valor_total, @tipo_venda, @data_venda)";
+                using (MySqlCommand cmd = new MySqlCommand(cmdSalvarVenda, _con))
+                {
+                    cmd.Prepare();
+                    cmd.
+                }
+            }
         }
     }
 }
