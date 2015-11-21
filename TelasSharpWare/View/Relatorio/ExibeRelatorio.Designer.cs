aa43a3776b4af0e62fdb6f1dbf216a9ad07cf288
Christopher Nicolas Mauricio <christopher.nicolas.mauricio@gmail.com>
2015-11-21 15:14:29 -0200

    Inicio de relatorio... CNM

---
+++ TelasSharpWare/View/Relatorio/ExibeRelatorio.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace TelasSharpWare.View.Relatorio
+{
+    partial class ExibeRelatorio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ExibeRelatorio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 253);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "ExibeRelatorio";
+            this.Text = "  Sport 4 You        -          Relatorio de Vendas por Cliente";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
